360b7ce012c3d29625efbc5da62c55c0677b0877
Arshin Jain <arshin.jain@outlook.com>
2017-07-09 07:59:12 -0700

    comments

--- Map-working/Udacity.swift
+++ Map-working/Udacity.swift
@@ -15,14 +15,17 @@ class Udacity: NSObject {
     // Login
     func login(username: String, password: String, completion: @escaping (_ success: Bool,_ errorMessage: String?, _ error: NSError?) -> Void) {
         
+        // Request
         let request = NSMutableURLRequest(url: NSURL(string: "https://www.udacity.com/api/session") as! URL)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}".data(using: String.Encoding.utf8)
         
+        // Session
         let session = URLSession.shared
         
+        // Task
         let task = session.dataTask(with: request as URLRequest) { (data, res, error) in
             
             func handleError(error: String, errorMessage: String) {
