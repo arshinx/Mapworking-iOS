18f8485d27c01c7b718d273f0af4c86edb53606f
Arshin Jain <arshin.jain@outlook.com>
2017-07-09 11:14:15 -0700

    All errors

--- Map-working/Errors.swift
+++ Map-working/Errors.swift
@@ -11,5 +11,10 @@ import Foundation
 struct Error {
     let DataError = "Error Getting Data!"
     let MapError = "Failed To Geocode!"
+    let UpdateError = "Failed To Update Location!"
+    let InvalidLink = "Invalid Link!"
+    let MissingLink = "Need To Enter Link!"
+    let CantLogin = "Network Connection Is Offline!"
+    let InvalidEmail = "Invalid Email Or Password!"
     
 }
