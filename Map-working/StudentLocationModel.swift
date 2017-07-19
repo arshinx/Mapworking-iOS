59802c226d434c74d7dd7e5066e5433529025d9c
Arshin Jain <arshinx@users.noreply.github.com>
2017-07-18 17:46:45 -0700

    Struct

---
+++ Map-working/StudentLocationModel.swift
@@ -0,0 +1,14 @@
+//
+//  StudentLocationModel.swift
+//  Map-working
+//
+//  Created by Arshin Jain on 7/18/17.
+//  Copyright © 2017 Arshin Jain. All rights reserved.
+//
+
+import Foundation
+
+struct StudentLocationModel {
+    
+    
+}
