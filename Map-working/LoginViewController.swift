483154ed3a6e96552ab6502d4bace09408fb01cd
Arshin Jain <arshinx@users.noreply.github.com>
2017-07-18 19:23:36 -0700

    LoginVC

--- Map-working/LoginViewController.swift
+++ Map-working/LoginViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LoginViewController: UIViewController {
 
     // MARK: Properties
+    
     // Outlets
     
     // Actions
@@ -18,23 +19,13 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
