ffff484b20728655b6d5267dbb3156e51498433d
Arshin Jain <arshinx@users.noreply.github.com>
2017-07-18 19:39:12 -0700

    dataSource

--- Map-working/LoginViewController.swift
+++ Map-working/LoginViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class LoginViewController: UIViewController {
 
     // MARK: Properties
-    let udacity = Udacity.sharedInstance()
+    let udacity     = Udacity.sharedInstance()
+    let dataSource  = DataSource.sharedDataSource()
     
     // Outlets
     @IBOutlet weak var emailField: UITextField!
