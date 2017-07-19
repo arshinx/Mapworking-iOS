028776b4e7e98abf06a35d07b6ed0c0f5cfde671
Arshin Jain <arshinx@users.noreply.github.com>
2017-07-18 19:32:07 -0700

    Add UIAlertController Extension - Create Alert

--- Map-working/Extensions.swift
+++ Map-working/Extensions.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 import UIKit
+
+extension UIAlertController {
+    
+    func createAlert(title: String, message: String) {
+        
+        // Create AlertController
+        let alert: UIAlertController = UIAlertController(title: title,
+                                                         message: message,
+                                                         preferredStyle: .alert)
+        
+        // Create Action
+        let action: UIAlertAction = UIAlertAction(title: "OK", style: .default)
+        
+        // Add Action
+        alert.addAction(action)
+        
+        // Present Alert
+        self.present(alert, animated: true)
+    }
+}
